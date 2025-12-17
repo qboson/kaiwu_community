d2a566bb7dad6b66dd8963c857bc52164ddebbb8
nixd <nixd@boseq.com>
2025-12-17 18:24:16 +0800

    更新版本号

--- src/kaiwu_community/__init__.py
+++ src/kaiwu_community/__init__.py
@@ -8,7 +8,7 @@
 import logging
 from kaiwu_community import qubo, core, ising, conversion, classical, solver
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 __all__ = ["qubo", "core","ising","conversion","classical","solver"]
